e3d7db989fca199e6818403eedc2686eb76b5f59
Oguz Kocer <oguz.kocer@automattic.com>
2022-04-25 15:54:33 -0400

    Adds kotlin & publishToS3 plugins to libs.versions.tomL

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,7 +1,13 @@
 val versionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.automattic.publishToS3).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
+    alias(libs.plugins.kotlin.android.extensions).apply(false)
+    alias(libs.plugins.kotlin.jvm).apply(false)
+    alias(libs.plugins.kotlin.kapt).apply(false)
 }
 
 android {
